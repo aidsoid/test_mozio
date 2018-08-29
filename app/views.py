a78994d3670153558b05a9dd94c8b8d82ded4971
Unknown <aidsoid@gmail.com>
2018-08-29 16:04:21 +0300

    Add API endpoints for Provider, ServiceArea, User and Group models

--- app/views.py
+++ app/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, mixins, generics
 
-# Create your views here.
+from app.models import Provider, ServiceArea
+from app.serializers import UserSerializer, GroupSerializer, ProviderSerializer, ServiceAreaSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows providers to be viewed or edited.
+    """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows service areas to be viewed or edited.
+    """
+    queryset = ServiceArea.objects.all()
+    serializer_class = ServiceAreaSerializer
+
+# class ProviderList(generics.ListCreateAPIView):
+#     """
+#     API endpoint that allows provider to be viewed or edited.
+#     """
+#     queryset = Provider.objects.all()
+#     serializer_class = ProviderSerializer
+#
+#
+# class ProviderDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     API endpoint that allows provider to be viewed or edited.
+#     """
+#     queryset = Provider.objects.all()
+#     serializer_class = ProviderSerializer
+#
+#
+# class ServiceAreaList(generics.ListCreateAPIView):
+#     """
+#     API endpoint that allows service area to be viewed or edited.
+#     """
+#     queryset = ServiceArea.objects.all()
+#     serializer_class = ServiceAreaSerializer
+#
+#
+# class ServiceAreaDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     API endpoint that allows service area to be viewed or edited.
+#     """
+#     queryset = ServiceArea.objects.all()
+#     serializer_class = ServiceAreaSerializer
--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import routers
+from app import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'providers', views.ProviderViewSet)
+router.register(r'service_areas', views.ServiceAreaViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    # # List all providers, or create a new provider
+    # path('providers/', views.ProviderList.as_view()),
+    # # Retrieve, update or delete a providers instance
+    # re_path('^providers/(?P<pk>[0-9]+)/$', views.ProviderDetail.as_view()),
+    # # List all ServiceArea, or create a new provider
+    # path('service_area/', views.ServiceAreaList.as_view()),
+    # # Retrieve, update or delete a ServiceArea instance
+    # re_path('^service_area/(?P<pk>[0-9]+)/$', views.ServiceAreaDetail.as_view()),
+    # Admin
     path('admin/', admin.site.urls),
 ]
