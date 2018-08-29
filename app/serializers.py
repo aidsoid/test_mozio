55d7e3be76a371da5a8eb3a76400ec5a1b33c619
Unknown <aidsoid@gmail.com>
2018-08-29 15:54:07 +0300

    Add serializers

---
+++ app/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from app.models import Provider, ServiceArea
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+
+class ProviderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Provider
+        fields = ('id', 'name', 'email', 'phone', 'language', 'currency', 'service_areas')
+
+
+class ServiceAreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceArea
+        fields = ('id', 'name', 'price', 'currency', 'poly')
